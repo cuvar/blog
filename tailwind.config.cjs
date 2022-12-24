c13e7b6018666eb21bfaa26d02f998602064fcaa
cuvar <luca-mueller@gmx.net>
2022-12-24 13:50:08 +0100

    feat: add color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				cgreen: {
+					300: "#5eadaa", // https://maketintsandshades.com/#198a86
+					400: "#47a19e",
+					500: "#198a86",
+					600: "#146e6b",
+					700: "#0f5350",
+					800: "#0d4543",
+					900: "#0a3736",
+				}
+			}
+		},
 	},
 	plugins: [],
 }
